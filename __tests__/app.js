8156364ca5a62ea55b662576766a2b7dc7c673cb
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-04-30 11:32:18 +0530

    tests #0

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,14 +3,19 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-bankai:app', () => {
+describe('generator-zanpakuto:app', () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+      .withPrompts({ 
+        type: "tmpl-express-js",
+        templateName: "express-hello-world",
+        description: "Express.js Hello world template",
+        pkgManager: "npm"
+       });
   });
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+    assert.file(['index.js', 'package.json']);
   });
 });
