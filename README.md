ee82bd388717fc2cdb53c99740bb1be7ddab7a6d
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-05-19 03:35:51 +0530

    express-hello-world template done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# generator-bankai [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
-> Bankai those templates onto folder structure.
+## Zanpakuto
+> Forge templates through scripts.
 
 ## Installation
 
@@ -16,21 +16,8 @@ Then generate your new project:
 yo zanpakuto
 ```
 
-## Getting To Know Yeoman
 
- * Yeoman has a heart of gold.
- * Yeoman is a person with feelings and opinions, but is very easy to work with.
- * Yeoman can be too opinionated at times but is easily convinced not to be.
- * Feel free to [learn more about Yeoman](http://yeoman.io/).
-
-## License
-
- © [Mridul Dhiman](https://github.com/MridulDhiman)
-
-
-[npm-image]: https://badge.fury.io/js/generator-bankai.svg
-[npm-url]: https://npmjs.org/package/generator-bankai
-[travis-image]: https://travis-ci.com/MridulDhiman/generator-bankai.svg?branch=master
-[travis-url]: https://travis-ci.com/MridulDhiman/generator-bankai
-[daviddm-image]: https://david-dm.org/MridulDhiman/generator-bankai.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org/MridulDhiman/generator-bankai
+## Functionalities: 
+- [x] Express hello world template
+- [ ] Express backend boilerplate
+- [ ] Next.js with nextauth boilerplate
--- app/index.js
+++ app/index.js
@@ -1,38 +1,74 @@
 'use strict';
-const  Generator = require('yeoman-generator');
+import  Generator from 'yeoman-generator';
+import path from "path";
 
-module.exports = class extends Generator {
+import { fileURLToPath } from 'url';
+import {kebabCase} from 'lodash-es'
+import { exec } from 'child_process';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+export default class extends Generator {
   constructor (args, opts) {
     super(args, opts);
-    // cli description
-  this.description = "Generates templates from CLI",
-
-  // --hello string flag...
-    this.option("hello", {alias: "h", type: String, description: "arigatou zanpakuto"})
+    this.options = opts;
+  this.description = "Generates templates from CLI"
+  this.env.options.nodePackageManager = "npm"
 }
 
 
-
-default () {
-  this.log("Hello from zanpakuto");
-}
-prompting () {
+async prompting () {
 
  //message hai jo bhi, add to result...
-
  
- 
-  this.prompt({
+ await this.prompt(
+    [ {
     type: "input",
-    name: "hello",
-    "message": "Enter message: ",
-    default: "hello from jon snow",
- }).then((ans) => {
-    this.log("Message: ", ans.hello)
-  })
+    name: "name",
+    "message": "Enter Directory name: ",
+    default: "zanpakuto",
+ }, {
+  type: "confirm",
+  name:"confirm",
+  message: "Do you want to use current directory as destination path: ",
+ }]).then((answers) => {
+  this.options.dirname = kebabCase(answers.name);
+  this.options.confirm = answers.confirm;
+});
 }
 
+writing() {
+
+if(this.options.confirm === false)
+   {
+    this.destinationRoot(this.options.dirname);
+    this.appname = this.options.dirname;
+   } 
+
+        this.log();
+        this.log(`Writing in ${this.destinationPath()}...`);
+this.sourceRoot(path.join(__dirname, "templates", "express-hello-world"));
+this.fs.copy(this.templatePath('index.js'), this.destinationPath('index.js'));
+this.fs.copy(this.templatePath("package.json"), this.destinationPath("package.json"));
+
+
+}
+
+async install () {
+  this.log.info("Installing npm packages...")
+await exec(`cd ${this.destinationPath()} && npm install`)
+}
+
+end() {
+  this.log("npm packages has been installed");
+  this.log("Template has been generated successfully...")
+}
 };
+
+
 
 
 
