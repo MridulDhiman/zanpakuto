e7054402a243c07d19fc0e6d8a402567f1ce1f00
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-05-22 15:55:58 +0530

    Feat: New next.js app router utility

--- README.md
+++ README.md
@@ -6,8 +6,7 @@
 First, install [Yeoman](http://yeoman.io) and generator-zanpakuto using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
 
 ```bash
-npm install -g yo
-npm install -g generator-zanpakuto
+npm install -g yo generator-zanpakuto
 ```
 
 Then generate your new project:
@@ -17,7 +16,58 @@ yo zanpakuto
 ```
 
 
+
 ### Templates: 
-- [x] Express hello world template.
-- [x] Express mongoose boilerplate.
-- [ ] Next.js app router
+-  Express hello world template
+-  Express mongoose boilerplate.
+
+### Utilities 
+-  [Next.js app router](#nextjs-app-router)
+
+## Usage:
+
+### Next.js app router
+
+1. Initialize next.js project with app router: 
+
+```javascript
+npm create-next-app@latest
+```
+
+2. Select utility option, after entering `yo zanpakuto`.
+3. Initialize a `routes.json` file in the next.js project root directory in this way: 
+
+```json
+{
+    "routes":
+     [
+        // normal routes
+     {
+        "path": "/signup",
+        "name": "Signup"
+     },
+     {
+        
+        "path": "/login",
+        "name": "Login"
+     },
+     {
+      "path": "/dashboard",
+      "name": "Dashboard"
+     },
+
+     // api routes
+     {
+      "path" : "/api/signup",
+      "name": "Signup Route"
+     },
+     {
+      "path" : "/api/login",
+      "name": "Login Route"
+     }
+    ]
+}
+```
+
+4. You can specify whether to use typescript and /src directory according to the project.
+
--- app/index.js
+++ app/index.js
@@ -4,12 +4,23 @@ import path from "path";
 
 import { fileURLToPath } from 'url';
 import {kebabCase} from 'lodash-es'
-import { exec } from 'child_process';
 import { globSync } from 'glob';
+import { readFileSync } from 'fs';
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const mappingConfigToPath = (data) => {
+  const routes = data.routes;
+
+  const paths =  [];
+  routes.forEach((route) => {
+    paths.push({path: route.path, name: route.name})
+  })
+
+  return paths;
+}
 
 
 export default class extends Generator {
@@ -24,72 +35,168 @@ export default class extends Generator {
 async prompting () {
 
  //message hai jo bhi, add to result...
- 
- await this.prompt(
-    [ {
-    type: "input",
-    name: "name",
-    "message": "Enter Directory name: ",
-    default: "zanpakuto",
- }, {
+ const templatePrompts =  [ 
+{
+type: "list",
+name: "type", 
+message: "What type of template you want to generate for yourself: ",
+choices: [
+  {
+    name: "express-hello-world",
+    value: "express-hello-world"
+  },
+  {
+    name: "express-mongoose-boilerplate",
+    value: "express-mongoose-boilerplate"
+  }
+]
+},
+{
+  type: "input",
+  name: "name",
+  "message": "Enter Directory name: ",
+  default: "zanpakuto",
+},
+{
   type: "confirm",
   name:"confirm",
   message: "Do you want to use current directory as destination path: ",
- },
+  }
+]
 
- {
-  type: "list",
-  name: "type", 
-  message: "What type of template you want to generate for yourself: ",
-  choices: [
-    {
-      name: "express-hello-world",
-      value: "express-hello-world"
-    },
-    {
-      name: "express-mongoose-boilerplate",
-      value: "express-mongoose-boilerplate"
-    }
-  ]
- }
-]).then((answers) => {
-  this.options.dirname = kebabCase(answers.name);
-  this.options.confirm = answers.confirm;
-  this.options.type = answers.type;
+
+const defaultPrompt = [
+  {
+    type : "list",
+    name : "promptType",
+    message: "Choose from below options: ",
+    choices: [
+        {
+          "name": "template",
+          "value": "template"
+        },
+        {
+          "name": "utility",
+          "value": "utility"
+        }
+    ]
+  }
+]
+
+const utilityPrompts = [
+  {
+    type: "input",
+    message : "Enter the next.js route config file name: ",
+    name: "routesFileName",
+    default: "routes.json"
+  }, 
+  {
+type :"confirm",
+message: "Would you like to use Typescript? ",
+name: "hasTs"
+  },
+  {
+    type: "confirm",
+    message: "Would you like to use /src directory? ",
+    name : "hasSrcDirectory"
+  }
+]
+
+ await this.prompt(defaultPrompt).then(async (ans) => {
+this.options.promptType = ans.promptType;
+
+
+if(this.options.promptType === "template") {
+  await this.prompt(templatePrompts).then((answers) => {
+    this.options.dirname = kebabCase(answers.name);
+    this.options.confirm = answers.confirm;
+    this.options.type = answers.type;
+  });
+}
+
+else {
+  await this.prompt(utilityPrompts).then((answers) => {
+    this.options.routesFileName = answers.routesFileName;
+this.options.hasTs = answers.hasTs;
+this.options.hasSrcDirectory = answers.hasSrcDirectory;
+  })
+}
+
+
+
 });
 }
 
 writing() {
 
-if(this.options.confirm === false)
-   {
-    this.destinationRoot(this.options.dirname);
-    this.appname = this.options.dirname;
-   } 
+  if(this.options.promptType === "template") {
+    if(this.options.confirm === false)
+      {
+       this.destinationRoot(this.options.dirname);
+       this.appname = this.options.dirname;
+      } 
+   
+      this.log();
+      this.log(`Writing in ${this.destinationPath()}...`);
+      this.sourceRoot(path.join(__dirname, "templates", this.options.type));
+      
+      
+      globSync("**", {
+        cwd: this.sourceRoot()
+      }).map((file) =>
+        this.fs.copyTpl(this.templatePath(file), this.destinationPath(file), {name: this.options.dirname})
+      );   
+    }
+    else {
+  this.env.options.skipInstall = true;
+const routesConfigFile = this.options.routesFileName;
 
-        this.log();
-        this.log(`Writing in ${this.destinationPath()}...`);
-this.sourceRoot(path.join(__dirname, "templates", this.options.type));
+const out = readFileSync(this.destinationPath(routesConfigFile));
+const data = JSON.parse(out);
 
 
-globSync("**", {
-  cwd: this.sourceRoot()
-}).map((file) =>
-  this.fs.copy(this.templatePath(file), this.destinationPath(file))
-);
+const paths = mappingConfigToPath(data);
+
+const hasTs = this.options.hasTs;
+const hasSrcDirectory = this.options.hasSrcDirectory;
+
+let sourceFile = hasTs ? "page.tsx" : "page.jsx";
+
+this.sourceRoot(path.join(__dirname, "templates", "nextjs-app-router"));
+
+
+const rootRoute = hasSrcDirectory ? "src/app" : "app";
+paths.forEach((p)=> {
+  let temp = p.path;
+  if(temp.startsWith("/api")) sourceFile = hasTs ? "route.ts" : "route.js";
+
+    this.fs.copyTpl(this.templatePath(sourceFile), this.destinationPath(path.join(rootRoute, p.path, sourceFile)), { name: p.name });
+
+  });
+
+
+}
 
 }
 
  install () {
-  this.log.info("Installing npm packages...")
-//  exec(`cd ${this.destinationPath()} && npm install`)
-this.spawnCommandSync("cd", [this.destinationPath()]);
-this.spawnCommandSync("npm", ['install']);
+
+  if(this.options.promptType === "template") {
+    this.log.info("Installing npm packages...")
+  this.spawnSync("cd", [this.destinationPath()]);
+  this.spawnSync("npm", ['install']);
+  }
 }
 
 end() {
-  this.log("npm packages has been installed");
-  this.log("Template has been generated successfully...")
+  if(this.options.promptType === "template")
+     {
+      this.log("npm packages has been installed");
+      this.log("Template has been generated successfully...")
+     } 
+     else {
+      this.log("Success: next.js app router routes have been added Successfully...")
+     }
 }
 };
 
